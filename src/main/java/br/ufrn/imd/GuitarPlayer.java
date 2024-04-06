df93bb2632ce89c748202e28802db48d64ea63ba
Beatriz <bia.s.moura9@gmail.com>
2024-04-06 18:19:14 -0300

    att

---
+++ src/main/java/br/ufrn/imd/GuitarPlayer.java
@@ -0,0 +1,16 @@
+package br.ufrn.imd;
+
+public class GuitarPlayer extends Musician {
+
+    public boolean howManyYears;
+    public boolean playEletronicGuitar;
+    public boolean playAcousticGuitar;
+
+
+    GuitarPlayer(String name, String hometown) {
+        super(name, hometown);
+    }
+    
+    public void playGuitar() {
+    }
+}
